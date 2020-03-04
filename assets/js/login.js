e8f8b139adcd3b84bcdebc09b0133a093a60d14b
keolite <e.baillysalins@outlook.fr>
2020-03-04 22:21:25 +0100

    Update - Gestion des messages d'erreurs
    Mise en évidence des erreurs des éléments obligatoire pour le formulaire de login

--- assets/js/login.js
+++ assets/js/login.js
@@ -5,7 +5,18 @@ const form = document.getElementsByTagName('form')[0];
 
 if( form !== null ){
     form.addEventListener('submit', (e)=>{
-        e.preventDefault();
-        console.log('lol');
+        const elements = form.elements;
+        for( let element of elements){
+            if( !element.validity.valid){
+                let elementParent = element.parentElement;
+                let elementMsgError = elementParent.querySelector('.valid-feedback');
+                if( elementMsgError !== null ){
+                    elementMsgError.classList.replace('valid-feedback', 'invalid-feedback');
+                    elementMsgError.classList.add('d-block');
+                }
+                e.preventDefault();
+            }
+        }
+
     });
 }
