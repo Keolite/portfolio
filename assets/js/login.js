ec35b7e4031cb64982d12114006a3e9bed418730
keolite <e.baillysalins@outlook.fr>
2020-03-05 22:26:14 +0100

    Update - Correction du scripts de soumission du formulaire

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,22 +1,44 @@
 import "./app";
 
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Sélection du formulaire de login ayant la class needs-validation
+        const form = document.getElementsByClassName('needs-validation')[0];
 
-const form = document.getElementsByTagName('form')[0];
+        //Tester si js à bien trouvé le formulaire
+        if( form !== null){
 
-if( form !== null ){
-    form.addEventListener('submit', (e)=>{
-        const elements = form.elements;
-        for( let element of elements){
-            if( !element.validity.valid){
-                let elementParent = element.parentElement;
-                let elementMsgError = elementParent.querySelector('.valid-feedback');
-                if( elementMsgError !== null ){
-                    elementMsgError.classList.replace('valid-feedback', 'invalid-feedback');
-                    elementMsgError.classList.add('d-block');
+            //Ajout de l'événement sur soumiision du formulaire
+            form.addEventListener('submit', function(event) {
+
+                //Sélection de tous les éléments du formulaire
+                const elements = form.elements;
+
+                //Vérifier si il y a des éléments à valider
+                if( elements.length > 0 ){
+
+                    //Pour chaque éléments si l'élément est invalide on ajoute la class invalid
+                    for( let element of elements){
+                        if( !element.validity.valid){
+                            let elementParent = element.parentElement;
+                            let elementMsgError = elementParent.querySelector('.valid-feedback');
+                            if( elementMsgError !== null ){
+                                elementMsgError.classList.replace('valid-feedback', 'invalid-feedback');
+                            }
+                        }
+                    }
                 }
-                e.preventDefault();
-            }
-        }
 
-    });
-}
+                //Si le formulaire n'est pas valide on stop la soumission
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                //on ajoute la class qui indique que le formulaire a été validé
+                form.classList.add('was-validated');
+            });
+        }
+    }, false);
+})();
