2d54dd50faa09fc477aad6c5625fca422409209d
keolite <e.baillysalins@outlook.fr>
2020-03-06 11:20:51 +0100

    Update - Formulaire de login.html.twig
    Ajuot des classes bootstrap pour les champs obligatoires. Refactoriastion du script de base de boostrap pour tester les valeurs du formulaire.

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,7 @@ import "./app";
 (function() {
     'use strict';
     window.addEventListener('load', function() {
+
         // Sélection du formulaire de login ayant la class needs-validation
         const form = document.getElementsByClassName('needs-validation')[0];
 
@@ -11,24 +12,6 @@ import "./app";
 
             //Ajout de l'événement sur soumiision du formulaire
             form.addEventListener('submit', function(event) {
-
-                //Sélection de tous les éléments du formulaire
-                const elements = form.elements;
-
-                //Vérifier si il y a des éléments à valider
-                if( elements.length > 0 ){
-
-                    //Pour chaque éléments si l'élément est invalide on ajoute la class invalid
-                    for( let element of elements){
-                        if( !element.validity.valid){
-                            let elementParent = element.parentElement;
-                            let elementMsgError = elementParent.querySelector('.valid-feedback');
-                            if( elementMsgError !== null ){
-                                elementMsgError.classList.replace('valid-feedback', 'invalid-feedback');
-                            }
-                        }
-                    }
-                }
 
                 //Si le formulaire n'est pas valide on stop la soumission
                 if (form.checkValidity() === false) {
