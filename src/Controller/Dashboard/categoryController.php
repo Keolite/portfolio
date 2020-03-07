ab1122a25009265a36e2eb221bb58f69e33abd6b
keolite <e.baillysalins@outlook.fr>
2020-03-07 21:26:13 +0100

    Add - Gestion des tecnologies
    Mise en place de la gestion des tehnologies et des catégories de la technologie

---
+++ src/Controller/Dashboard/categoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Dashboard;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/ebs-admin/categorie")
+ */
+class categoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="dashboard_category")
+     */
+    public function index()
+    {
+        return $this->render('dashboard/category/index.html.twig', [
+            'controller_name' => 'categoryController',
+        ]);
+    }
+}
